583c69a15621c7d72f456e5710b9147131997f52
jheslop25 <john@earthmountain.ca>
2020-02-18 20:53:22 +0000

    created DB migrations

---
+++ database/migrations/2020_02_18_002717_create_attacks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttacksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attacks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('pokemon_type');
+            $table->string('attack_name');
+            $table->bigInteger('strength');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attacks');
+    }
+}
